adab1122af4e5c71404c3bf30eaea73fa21e4357
Jiah Pang <jiah@Pang-iMac.local>
2018-01-28 13:23:07 +1100

    Not working, for light reading
    Have fun Ollie :)

--- src/org/usfirst/frc/team6579/robot/subsystem/Drivetrain.java
+++ src/org/usfirst/frc/team6579/robot/subsystem/Drivetrain.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team6579.robot.subsystem;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -9,7 +12,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * The drivetrain consists of two toughbox minis (7.31:1) with two CIM motors on each side. Thr drivetrain may also have an encoder or two to measure distances, and a gyro.
  *
  * PORTS:
- * ToDo: fill ports for drivetrain
+ * Motors in port 0,1,2,3 PWM RIO
+ * ToDo: check ports for drivetrain
  *
  * The drivetrain will be solely controlled by the driver
  *
@@ -17,9 +21,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Drivetrain implements SubSystem {
 
-    //Define drivetrain toughbox minis
-    private Toughbox toughbox1 = new Toughbox(0,1);
-    private Toughbox toughbox2 = new Toughbox(2,3);
+//    //Define drivetrain toughbox minis
+//    private Toughbox toughbox1 = new Toughbox(0,1);
+//    private Toughbox toughbox2 = new Toughbox(2,3);
+
+
+    private SpeedControllerGroup toughbox1 = new SpeedControllerGroup(0,1);
+
+    private VictorSP m_frontRight = new VictorSP(2);
+    private VictorSP m_rearRight = new VictorSP(3);
+    private SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+
 
     //left drive set
     private Toughbox leftToughbox = toughbox1;
@@ -28,6 +40,9 @@ public class Drivetrain implements SubSystem {
 
     //gyro
     private ADXRS450_Gyro gyro = null;
+
+    //defining the drivetrain type
+    private DifferentialDrive robotDrive = new DifferentialDrive(leftToughbox,rightToughbox);
 
     /**
      * Declaration of class, tries for gyro and sets the gear side as the default front of robot
@@ -61,7 +76,127 @@ public class Drivetrain implements SubSystem {
         rightToughbox.set(rightPower);
     }
 
+    /**
+     * This method is to stop the power going to the robot.
+     * This does NOT make the robot do a hard stop unless brake is set on the motor controllers
+     */
+    public void stop(){
+        setPower(0,0);
+    }
 
+    /**
+     * This method makes the robot stop on the spot.
+     */
+    public void hardStop(){
+        double leftStopPower = 0.1;
+        double rightStopPower = 0.1;
+
+        if (leftToughbox.get()>0){
+            leftStopPower = -0.1;
+
+        }
+        else{
+            leftStopPower = 0.1;
+        }
+
+        if (rightToughbox.get() > 0){
+            rightStopPower = -0.1;
+        }
+        else{
+            rightStopPower = 0.1;
+        }
+        //SmartDashboard.putNumber("leftStopPower",leftStopPower);
+        //SmartDashboard.putNumber("rightStopPower",rightStopPower);
+        setPower(leftStopPower,rightStopPower);
+
+    }
+
+    /**
+     * Gets the current gyro angle
+     */
+    public double getGyroAngle(){
+        publishStats();
+        double gyroAngle = 0;
+
+        //error handling for if there is no gyro
+        if (gyro != null){
+            gyroAngle = gyro.getAngle();
+        }
+        return gyroAngle;
+    }
+
+    /**
+     * Gets the moderated gyro angle (makes values between -360 and 360)
+     * @return
+     */
+    public double getModGyroAngle(){
+        double gyroModAngle = getGyroAngle() % 360;
+        SmartDashboard.putNumber("getModGyroAngle", gyroModAngle);
+
+        return gyroModAngle;
+    }
+
+    /**
+     * resets the gyro
+     */
+    public void resetGyro(){gyro.reset();}
+
+    /**
+     * Follows an angle off the gyro at a driver defined speed
+     * @param power
+     * @param gyroTarget
+     */
+    public void followGyro(double power, double gyroTarget)
+    {
+        // ToDo: fill in this method
+        //proportionally drives in the direction of a gyro heading, turning to face the right direction
+        double currentGyroAngle = getGyroAngle() % 360;
+        double gyroPowerAdjustment = 0;
+        double gyroGain = 0.05;
+
+
+        //Calculates how much to turn based on the current heading and the target heading
+        gyroPowerAdjustment = currentGyroAngle - (gyroTarget % 360);
+        gyroPowerAdjustment = gyroPowerAdjustment * gyroGain;
+
+        double gyroMotorPowerLeft = power - gyroPowerAdjustment;
+        double gyroMotorPowerRight = -power - gyroPowerAdjustment; //ToDo: Make adjustment to remove the - from in front when changing to setPower
+
+        //Makes the motors move
+        leftToughbox.set(gyroMotorPowerLeft);
+        rightToughbox.set(gyroMotorPowerRight);
+    }
+    /**
+     * Turns left if "left" is true otherwise turns right until the robot reaches the target angle.
+     * @param targetAngle
+     * @param left
+     */
+    public void gyroTurn(double targetAngle, boolean left)
+    {
+        double turnPower = 0.25;
+
+        SmartDashboard.putNumber("gyroTurn.targetAngle", targetAngle);
+        SmartDashboard.putBoolean("gyroTurn.left", left);
+
+        // reset gyro sensor to zero
+        gyro.reset();   // do not calibrate as this will stop the world and make the gyro crazy
+
+        while ( Math.abs(getModGyroAngle()) < Math.abs(targetAngle))
+        {
+            if (left)
+            {
+                setPower(-turnPower, turnPower);
+            }
+            else
+            {
+                // must want to turn right
+                setPower(turnPower, -turnPower);
+            }
+        }
+        hardStop();
+        stop();
+
+    }
 
     @Override
     public void publishStats() {
