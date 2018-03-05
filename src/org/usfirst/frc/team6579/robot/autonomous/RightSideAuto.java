3179081643da4afb251a0f4f71eb065ef60b65ec
Jiah Pang <jiah@pang.net.au>
2018-03-06 09:38:33 +1100

    Copypasta to righthandside
    Copied and inverted left hand side code

--- src/org/usfirst/frc/team6579/robot/autonomous/RightSideAuto.java
+++ src/org/usfirst/frc/team6579/robot/autonomous/RightSideAuto.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team6579.robot.autonomous;
 
 import org.usfirst.frc.team6579.robot.Robot;
+import org.usfirst.frc.team6579.robot.subsystem.Drivetrain;
+import org.usfirst.frc.team6579.robot.subsystem.Intake;
+import org.usfirst.frc.team6579.robot.subsystem.Lift;
 
 public class RightSideAuto extends AutoStrategy {
 
@@ -14,40 +17,92 @@ public class RightSideAuto extends AutoStrategy {
     @Override
     public void doStrategy(Robot robot) {
 
+        //Just copypasta from leftsideauto, double check if possible
         //Assuming robot is lined up with very far corner of base plate
 
-        if (!isScaleLeft()) {
-            logger.info("Switch is to the left and scale is to the right");
-            robot.getDrivetrain().driveStraight(360,0.40);//drives to side of switch //ToDo: find good power that works
-            robot.getDrivetrain().turn(90,true);//rotates to face switch
-            robot.getDrivetrain().driveStraight(8.5, 0.40);//drives closer to switch //ToDo: find good power that works
-            robot.getLift().raiseToHeight(200);//lifts lift 2m
-            robot.getIntake().intakeSpitOut(1);//spits out cube
+        Drivetrain drivetrain = robot.getDrivetrain();
+        Intake intake = robot.getIntake();
+        Lift lift = robot.getLift();
+
+
+        if (!isSwitchLeft()){
+            logger.info("Scale is to the right and Switch is to the left");
+            drivetrain.driveStraight(15, 40);
+            drivetrain.hardStop();
+            sleep(750);  // to give the cube time to drop
+
+            lift.raiseToHeight(20);
+
+            drivetrain.driveStraight(425,0.60);//drives to side of switch //ToDo: find good power that works
+            drivetrain.hardStop();
+            drivetrain.turn(90,true);//rotates to face switch
+            lift.raiseToHeight(80);//lifts lift to the 1m mark
+
+            // robot.getDrivetrain().driveStraight(35.5, 0.40);//drives closer to switch //ToDo: find good power that works
+
+            //make drive for seconds
+            long beginTimeToSwitch = System.currentTimeMillis();
+            while (System.currentTimeMillis()-beginTimeToSwitch < 2000) {
+
+                drivetrain.setPower(0.5,0.5);
+            }
+
+            intake.intakeSpitOut(1);//spits out cube
             sleep(1000);
-            robot.getIntake().stopIntake();//stops intake
+            intake.stopIntake();//stops intake
+
+            //stretch component
+
+
+
+
         }
-        else if (!isSwitchLeft()){
-            logger.info("Switch is to the right");
-            robot.getDrivetrain().driveStraight(360,0.40);//drives to side of switch //ToDo: find good power that works
-            robot.getDrivetrain().turn(90,true);//rotates to face switch
-            robot.getDrivetrain().driveStraight(35.5, 0.40);//drives closer to switch //ToDo: find good power that works
-            robot.getLift().raiseToHeight(100);//lifts lift 1m
-            robot.getIntake().intakeSpitOut(1);//spits out cube
+        else if (!isScaleLeft()) {
+            logger.info("Scale is to the left");
+            drivetrain.driveStraight(700,0.40);//drives to side of switch //ToDo: find distances
+            drivetrain.turn(30,true);//rotates to face switch
+            drivetrain.driveStraight(845, 0.40);//drives closer to switch //ToDo: find good power that works
+            lift.raiseToHeight(200);//lifts lift 2m
+            intake.intakeSpitOut(1);//spits out cube
             sleep(1000);
-            robot.getIntake().stopIntake();//stops intake
+            intake.stopIntake();//stops intake
+
+            //stretch
+            drivetrain.driveStraight(50,-40);//check that backwards works for this
+            lift.liftDown(1);
+            sleep(3000);
+
+            //extra stretch
+            drivetrain.turn(100,true);
+            long beginTimeHardStop = System.currentTimeMillis();
+            while (System.currentTimeMillis()-beginTimeHardStop < 3000) {
+
+                drivetrain.setPower(0.5,0.5);
+                intake.intakeSuckIn(1);
+            }
+
+
         }
+
         else{
             //drive past baseline
-            logger.info("Switch & Scale are to the left... doing baseline");
-            robot.getDrivetrain().driveStraight(415,0.40);//ToDo: find good power that works
-            robot.getDrivetrain().turn(90,true);
-            robot.getDrivetrain().driveStraight(523.5, 0.40);
-            robot.getDrivetrain().turn(90, true);
-            robot.getDrivetrain().driveStraight(140.5, 0.40);
-            robot.getLift().raiseToHeight(100);
-            robot.getIntake().intakeSpitOut(1);
-            sleep(1000);
-            robot.getIntake().stopIntake();
+//            logger.info("Switch & Scale are to the left... doing baseline");
+//            robot.getDrivetrain().driveStraight(415,0.40);//ToDo: find good power that works
+//            robot.getDrivetrain().turn(90,false);
+//            robot.getDrivetrain().driveStraight(523.5, 0.40);
+//            robot.getDrivetrain().turn(90, false);
+//            robot.getDrivetrain().driveStraight(140.5, 0.40);
+//            robot.getLift().raiseToHeight(100);
+//            robot.getIntake().intakeSpitOut(1);
+//            sleep(1000);
+//            robot.getIntake().stopIntake();
+
+            long beginTimeHardStop = System.currentTimeMillis();
+            while (System.currentTimeMillis()-beginTimeHardStop < 3000) {
+
+                drivetrain.setPower(0.5,0.5);
+            }
+
         }
 
         System.out.println("Right Side Auto complete...");
